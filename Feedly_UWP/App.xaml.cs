716db9118abee4070219cd01f212cf3076605b33
Fox <Fox@FOXNETPC>
2016-10-17 09:52:51 +0200

    Add project files.

---
+++ Feedly_UWP/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+
+using Prism.Autofac.Windows;
+
+using Prism.Windows;
+
+namespace Feedly_UWP
+{
+    
+    sealed partial class App : PrismAutofacApplication
+    {
+
+        public App()
+        {
+            this.InitializeComponent();
+        }
+        protected override Task OnLaunchApplicationAsync(LaunchActivatedEventArgs args)
+        {
+            NavigationService.Navigate("Main", null);
+            return Task.FromResult<object>(null);
+        }
+
+    }
+}
